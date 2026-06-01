40f24cbe866e1f1ddc59d3d85d80352e15cacf2b
Eryck fernando <eryck.barbosa@aluno.ce.gov.br>
2026-06-01 10:57:32 -0300

    Add App.jsx component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FlagAnimation from './components/FlagAnimation';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FlagAnimation />
+    </div>
+  );
+}
+
+export default App;
